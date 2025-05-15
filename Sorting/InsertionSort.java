7ca1ce2be39445036ced534aaedddbd0308fc8d3
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-05-16 01:06:28 +0530

    completed insertion sort

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,28 @@
+/**
+ * FOR J=2 TO A.LENGTH
+ *  KEY=A[J]
+ *  // INSERT A[J] INTO THE SORTED ARRAY A[1..J-1]
+ *  I=J-1
+ *  WHILE I>0 AND A[I]>KEY
+ *      A[I+1] = A[I]
+ *      I=I-1
+ *  A[I+1]=KEY
+ * 
+ */
+public class InsertionSort {
+    public int[] sort(int[] array) {
+
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+
+            int j = i;
+            while (j > 0 && array[j - 1] > temp) {
+                array[j] = array[j - 1];
+                --j;
+            }
+            array[j] = temp;
+        }
+
+        return array;
+    }
+}
