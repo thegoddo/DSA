2707ca064ec176eee4e06d242c9702e6d85d2cb6
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-05-16 15:39:46 +0530

    added display method

--- Sorting/Const.java
+++ Sorting/Const.java
@@ -4,4 +4,10 @@ public class Const {
         array[i] = array[j];
         array[j] = temp;
     }
+
+    public static final void display(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 }
