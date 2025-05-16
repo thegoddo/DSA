11451c59e33dd43def8e95e69d0d162a6c3468fb
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-05-16 15:39:57 +0530

    updated the switch cases

--- Sorting/Main.java
+++ Sorting/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("What typ of Sort?");
-        System.out.println("\n1.Bubble\n2.Selection");
+        System.out.println("\n1.Bubble\n2.Selection\n3.Merge\n4.Quick\n5.Insertion\n6.Shell\n7.Heap");
         int choice = Integer.parseInt(sc.nextLine());
 
         for (int i = 0; i < array.length; i++) {
@@ -16,18 +16,45 @@ public class Main {
 
         switch (choice) {
             case 1:
+
                 Bubble b = new Bubble();
                 b.sort(array);
-                for (int i = 0; i < array.length; i++) {
-                    System.out.print(array[i] + " ");
-                }
+                Const.display(array);
                 break;
+
             case 2:
+
                 SelectionSort selectionSort = new SelectionSort();
                 selectionSort.sort(array);
-                for (int i = 0; i < array.length; i++) {
-                    System.out.print(array[i] + " ");
-                }
+                Const.display(array);
+
+            case 3:
+                MergeSort mergeSort = new MergeSort();
+                mergeSort.mergeSort(array);
+                Const.display(array);
+                break;
+            case 4:
+                QuickSort quickSort = new QuickSort();
+                quickSort.sort(array, 0, array.length);
+                Const.display(array);
+                break;
+
+            case 5:
+                InsertionSort insertionSort = new InsertionSort();
+                insertionSort.sort(array);
+                Const.display(array);
+                break;
+
+            case 6:
+                ShellSort shellSort = new ShellSort();
+                shellSort.sort(array);
+                Const.display(array);
+                break;
+            case 7:
+                HeapSort heapSort = new HeapSort();
+                heapSort.heapSort(array);
+                Const.display(array);
+                break;
 
             default:
                 break;
