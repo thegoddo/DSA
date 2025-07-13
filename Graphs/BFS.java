a9302d9060bbb819c83ae6435d4a5f6261671b9f
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-07-13 23:25:32 +0530

    complete BFS algorithm

---
+++ Graphs/BFS.java
@@ -0,0 +1,44 @@
+package Graphs;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import org.w3c.dom.Node;
+
+public class BFS {
+
+    private class Node {
+        public Node left;
+        public Node right;
+        public int value;
+
+        public Node(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+
+        public void visit() {
+            System.out.println("Visited node with value: " + value);
+        }
+    }
+
+    public void breadthFirstSearch(Node root) {
+        if (root != null) {
+            Queue<Node> queue = new ArrayDeque<>();
+            queue.add(root);
+
+            while (!queue.isEmpty()) {
+                Node node = queue.remove();
+                node.visit();
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+    }
+}
